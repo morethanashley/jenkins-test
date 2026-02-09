e1c80c9d033a28fdf6c837bd1a0ffb4f40c2d087
morethanashley <ashleyferns0407@gmail.com>
2026-02-09 15:17:41 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,19 @@
 package com.example;
 
 /**
- * Hello world!
- *
+ * Hello world with addition example
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum: " + sum);
     }
 }
